05d715f4c4d3dff9a27493c1761342a1bbe0ca01
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 22:47:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,5 +12,6 @@ end
 
 def pop_with_args(dog_breeds)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  small_dogs = dog_breeds.pop(2)
+  small_dogs = ["Chihuahua", "Shiba Inu"]
+  small_dogs.pop(2)
 end
