f0a44174cdbe00c1e0f7a264beffeca87a20dc53
Jarrell Jackson <jdjack3@gmail.com>
2020-07-13 23:06:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,9 +13,14 @@ end
 def pop_with_args(dog_breeds)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   small_dogs = ["Chihuahua", "Shiba Inu"]
-  dog_breeds.pop
+  dog_breeds.pop(small_dogs)
 end
 
 def using_shift(array)
   array.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  brands_removed = ice_cream_brands.shift(2)
 end
